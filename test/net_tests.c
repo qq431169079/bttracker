1cd9a0cfedc603dd1fdd478e767c456c22047a82
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-06-09 14:06:35 -0300

    Added tests for `bt_local_addr` and `bt_conn_resp_to_network`.

--- test/net_tests.c
+++ test/net_tests.c
@@ -1,12 +1,20 @@
 #include "minunit.h"
 
-char *test_bt_req_from_network() {
-  int32_t net_int = htonl(3);
-  int64_t net_lng = htonll(3);
+char *test_bt_local_addr() {
+  unsigned short port = 321;
+  struct sockaddr_in addr = bt_local_addr(port);
 
-  bt_req_t req = {.connection_id = net_lng,
-                  .action = net_int,
-                  .transaction_id = net_int};
+  mu_assert("ip socket", addr.sin_family == AF_INET);
+  mu_assert("socket port", addr.sin_port == htons(port));
+  mu_assert("incomming address", addr.sin_addr.s_addr == htonl(INADDR_ANY));
+
+  return NULL;
+}
+
+char *test_bt_req_from_network() {
+  bt_req_t req = {.connection_id = htonll(3),
+                  .action = htonl(3),
+                  .transaction_id = htonl(3)};
 
   bt_req_from_network(&req);
 
@@ -17,8 +25,24 @@ char *test_bt_req_from_network() {
   return NULL;
 }
 
+char *test_bt_conn_resp_to_network() {
+  bt_connection_resp_t resp = {.action = 3,
+                               .transaction_id = 3,
+                               .connection_id = 3};
+
+  bt_conn_resp_to_network(&resp);
+
+  mu_assert("a", resp.action == htonl(3));
+  mu_assert("b", resp.transaction_id == htonl(3));
+  mu_assert("c", resp.connection_id == htonll(3));
+
+  return NULL;
+}
+
 char *all_tests() {
+  mu_run_test(test_bt_local_addr);
   mu_run_test(test_bt_req_from_network);
+  mu_run_test(test_bt_conn_resp_to_network);
 
   return NULL;
 }
