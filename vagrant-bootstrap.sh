62fdcab82cbfe46053ff92130ee6364f354fb28c
Daniel Fernandes Martins <daniel.tritone@gmail.com>
2013-06-09 21:44:13 -0300

    Added `valgrind` as a dependency.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y build-essential autoconf automake pkg-config libglib2.0-0 libglib2.0-dev
+apt-get install -y build-essential autoconf automake pkg-config libglib2.0-0 libglib2.0-dev valgrind
