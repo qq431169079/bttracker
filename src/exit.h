1567a7f39bd52befee6aecdf9fe5bf524dda6f4c
Daniel Martins <daniel.tritone@gmail.com>
2013-06-15 16:23:27 -0300

    Created enum with all possible exit codes.

--- src/exit.h
+++ src/exit.h
@@ -28,61 +28,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef BTTRACKER_ALLHEADS_H_
-#define BTTRACKER_ALLHEADS_H_
+#ifndef BTTRACKER_EXIT_H_
+#define BTTRACKER_EXIT_H_
 
-#include <stdio.h>
-#include <sys/time.h>
+/* Exit codes. */
+typedef enum {
+  BT_EXIT_OK,
+  BT_EXIT_NETWORK_ERROR,
+  BT_EXIT_MALLOC_ERROR
+} bt_exit;
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#ifdef HAVE_INTTYPES_H
-#include <inttypes.h>
-#endif
-
-#ifdef STDC_HEADERS
-#include <stdlib.h>
-#endif
-
-#ifdef HAVE_STDBOOL_H
-#include <stdbool.h>
-#endif
-
-#ifdef HAVE_UNISTD_H
-#include <unistd.h>
-#include <sys/types.h>
-#endif
-
-#ifdef HAVE_SYSLOG_H
-#include <syslog.h>
-#endif
-
-#ifdef HAVE_SYS_SOCKET_H
-#include <sys/socket.h>
-#endif
-
-#ifdef HAVE_ARPA_INET_H
-#include <arpa/inet.h>
-#endif
-
-#ifdef HAVE_NETINET_IN_H
-#include <netinet/in.h>
-#endif
-
-#ifdef HAVE_PTHREAD
-#include <pthread.h>
-#endif
-
-#include <glib.h>
-
-/* Application headers. */
-#include "byteorder.h"
-#include "random.h"
-#include "net.h"
-#include "ttl.h"
-#include "connect.h"
-#include "exit.h"
-
-#endif // BTTRACKER_ALLHEADS_H_
+#endif // BTTRACKER_EXIT_H_
